509bec1c053459d517371d647e10b345340d4172
Florent <Florent@DESKTOP-0A1423U>
2018-04-11 15:25:00 +0000

    ADD ERROR VIEW

---
+++ src/main/java/quantum/mutex/backing/ConfigBacking.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quantum.mutex.backing;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import quantum.mutex.service.ConfigService;
+
+/**
+ *
+ * @author Florent
+ */
+@Named(value = "configBacking")
+@ViewScoped
+public class ConfigBacking extends BaseBacking{
+    
+    @Inject ConfigService configService;
+    
+    public void buoldIndex(){
+        configService.buildIndex();
+    }
+}
---
+++ src/main/java/quantum/mutex/service/ConfigService.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quantum.mutex.service;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.hibernate.search.jpa.FullTextEntityManager;
+import org.hibernate.search.jpa.Search;
+
+/**
+ *
+ * @author Florent
+ */
+@Stateless
+public class ConfigService {
+    
+    @PersistenceContext
+    protected EntityManager em;
+    
+    public void buildIndex(){
+        try {
+            FullTextEntityManager fullTextEntityManager = Search.getFullTextEntityManager(em);
+            fullTextEntityManager.createIndexer().startAndWait();
+        } catch (InterruptedException ex) {
+            Logger.getLogger(ConfigService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
